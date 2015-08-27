9972e2c2f2f4f204f7403aeefce341afb089495f
Robie <robie1373@gmail.com>
2015-08-27 08:33:38 -0400

    Chaos. moved everything up a folder because the extra level annoyed me.

---
+++ didwhat/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for didwhat project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "didwhat.settings")
+
+application = get_wsgi_application()
