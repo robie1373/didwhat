495f258f2087aaf2178657b84195314c1501db6a
Robie <robie1373@gmail.com>
2015-08-28 13:09:23 -0400

    oops formatting

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -14,15 +14,15 @@ def deploy():
   _update_static_files(source_folder)
   _update_database(source_folder)
 
-  def dev-deploy():
-  site_folder = 'home/%s/sites/%s' % (env.user, env.host)
-  source_folder = site_folder + '/source'
-  _create_directory_structure_if_necessary(site_folder)
-  _get_latest_source(source_folder)
-  # _update_settings(source_folder, env.host)
-  _update_virtualenv(source_folder)
-  _update_static_files(source_folder)
-  _update_database(source_folder)
+  def dev_deploy():
+    site_folder = 'home/%s/sites/%s' % (env.user, env.host)
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    # _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
 
   def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
