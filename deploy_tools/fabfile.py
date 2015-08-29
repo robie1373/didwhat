4dec22634ef6ae76a439b6e2342e69f4c7df520d
ROBERT LUTSEY <robie@ROBERTs-MBP.fios-router.home>
2015-08-28 22:38:50 -0400

    fine. have your stoopid leading slashes.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,7 +5,7 @@ import random
 REPO_URL = 'https://github.com/robie1373/didwhat.git'
 
 def deploy():
-  site_folder = 'home/%s/sites/%s' % (env.user, env.host)
+  site_folder = '/home/%s/sites/%s' % (env.user, env.host)
   source_folder = site_folder + '/source'
   _create_directory_structure_if_necessary(site_folder)
   _get_latest_source(source_folder)
@@ -40,7 +40,7 @@ def _update_settings(source_folder, site_name):
   settings_path = source_folder + '/didwhat/settings.py'
   sed(settings_path, "DEBUG = True", "DEBUG = False")
   sed(settings_path, 'ALLOWED_HOSTS =.+$', 'ALLOWED_HOSTS = ["%s"]' %(site_name,))
-  secret_key_file = source_folder + 'didwhat/secret_key.py'
+  secret_key_file = source_folder + '/didwhat/secret_key.py'
   if not exists(secret_key_file):
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
     key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
